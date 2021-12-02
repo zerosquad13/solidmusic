3680e81c3ed4a601785e3715a8ef78ab222e7be5
zero <94368175+zerosquad13@users.noreply.github.com>
2021-12-02 14:11:09 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,9 +17,9 @@ class Configs:
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
     OWNER_ID = int(getenv("OWNER_ID", "0123"))
     SESSION = getenv("SESSION", "session")
-    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/solidprojects")
-    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/solidprojects_chat")
-    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
+    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/xproject13")
+    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/xxxsquad18plus")
+    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/zerosquad13/solidmusic")
     AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1800"))
 
 
